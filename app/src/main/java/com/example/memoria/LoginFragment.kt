e3466f9227c1959f5c3665cda1786d53ff01e975
nicholasc861 <nicholas.chunghun@gmail.com>
2023-03-11 16:56:18 -0500

    fix navigation issues

--- app/src/main/java/com/example/memoria/LoginFragment.kt
+++ app/src/main/java/com/example/memoria/LoginFragment.kt
@@ -5,11 +5,14 @@ import android.text.SpannableString
 import android.text.Spanned
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.memoria.databinding.FragmentLoginBinding
 
@@ -37,7 +40,44 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val register_prompt = SpannableString("Don't have an account? Register")
+        setupRegisterPrompt(view)
+
+
+
+
+        binding.loginButton.setOnClickListener() {
+            val toast = Toast.makeText(context, "Please enter username and/or password!", Toast.LENGTH_LONG)
+            val isNotEmpty = checkFieldValues(view)
+
+            if (isNotEmpty) {
+                toast.cancel()
+                findNavController().navigate(R.id.action_AuthFragment_to_FeedFragment)
+            } else {
+                toast.show()
+            }
+        }
+    }
+
+    private fun checkFieldValues(view: View) : Boolean {
+        val usernameField = view.findViewById<EditText>(R.id.loginUsername)
+        val passwordField = view.findViewById<EditText>(R.id.loginPassword)
+        val username = usernameField.text.toString()
+        val password = passwordField.text.toString()
+
+        // Check for valid characters in username
+
+        // Check for valid characters in password
+
+        // Check if username and password are non-empty
+        if (username.isNotEmpty() && password.isNotEmpty()){
+            return true
+        }
+
+        return false
+    }
+
+    private fun setupRegisterPrompt(view: View) {
+        val registerPrompt = SpannableString("Don't have an account? Register")
         val registerClickable : ClickableSpan = object : ClickableSpan() {
             override fun onClick(p0: View) {
                 findNavController().navigate(R.id.action_LoginFragment_to_registerFragment)
@@ -45,16 +85,11 @@ class LoginFragment : Fragment() {
             }
         }
 
-        register_prompt.setSpan(registerClickable, 23, 31, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        registerPrompt.setSpan(registerClickable, 23, 31, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
         val textView = view.findViewById<TextView>(R.id.registerPrompt)
-        textView.text = register_prompt
+        textView.text = registerPrompt
         textView.movementMethod = LinkMovementMethod.getInstance()
-
-
-        binding.loginButton.setOnClickListener() {
-            findNavController().navigate(R.id.action_AuthFragment_to_FeedFragment)
-        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/memoria/RegisterFragment.kt
+++ app/src/main/java/com/example/memoria/RegisterFragment.kt
@@ -48,23 +48,9 @@ class RegisterFragment : Fragment() {
         }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
     }
 }
