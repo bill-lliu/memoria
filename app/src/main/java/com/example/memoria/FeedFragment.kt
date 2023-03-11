1a4f786966f008bddffe315387a2dcd6e34ed9d1
siyaochen <siyaochen416@gmail.com>
2023-03-10 20:35:15 -0500

    Add search feature in feed

--- app/src/main/java/com/example/memoria/FeedFragment.kt
+++ app/src/main/java/com/example/memoria/FeedFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.fragment.findNavController
 import com.example.memoria.databinding.FragmentFeedBinding
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -19,12 +21,20 @@ class FeedFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var allPosts: List<Post>? = null
+    private var filteredPosts: List<Post>? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         _binding = FragmentFeedBinding.inflate(inflater, container, false)
+
+        loadPosts()
+
+        setupSearchView()
+
         return binding.root
 
     }
@@ -44,4 +54,35 @@ class FeedFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun loadPosts() {
+        // TODO: Load posts here
+
+        allPosts = listOf<Post>()
+        filteredPosts = allPosts
+    }
+
+    private fun setupSearchView() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                filteredPosts = getPosts(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun getPosts(tag: String?): List<Post> {
+        if (tag == null || allPosts == null) {
+            return allPosts ?: listOf<Post>()
+        }
+
+        val cleanedTag = tag.trim().lowercase()
+
+        return allPosts!!.filter { post -> post.tags.contains(cleanedTag) }
+    }
+
 }
---
+++ app/src/main/java/com/example/memoria/Post.kt
@@ -0,0 +1,6 @@
+package com.example.memoria
+
+// Temporary class representing a Post object - feel free to change
+class Post(val title: String, val description: String, val tags: List<String>) {
+    // TODO: Add image to Post and store posts in app
+}
